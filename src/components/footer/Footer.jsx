cd6261a497b67cc40bb6d0c25e769bc4ef104182
Avanish Kumar Gupta <91612116+Avanish-Gupta-CSE@users.noreply.github.com>
2023-06-07 10:26:26 +0530

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -18,7 +18,7 @@ export const Footer = () => {
                 </Link>
                 <Link
                     to={
-                        "https://mail.google.com/mail/u/0/#inbox?compose=CllgCJlDSvZZfTwPZQWvVjDGrSxzsdCXBGBPBzBCQSdMwpgkfcNRWwQmgBKmRXjKFwFsZtnGGRL"
+                        "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=avanish.gupta.official1@gamil.com"
                     }
                     target="blank"
                 >
